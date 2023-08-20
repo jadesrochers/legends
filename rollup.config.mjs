031c7da804913289f45897eca73369784a3a653c
jadesrochers <jadesrochers@gmail.com>
2023-08-20 15:44:53 -0400

    Get emotion removal completed

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,11 +3,11 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from "@rollup/plugin-terser";
 import filesize from 'rollup-plugin-filesize';
-
+import postcss from 'rollup-plugin-postcss';
 
 export default {
-    input: './src/index.js',
-    external: ['react', '@emotion/react', '@jadesrochers/histograminteract', 'd3-format', 'ramda' ],
+    input: './src/index.mjs',
+    external: ['react', '@jadesrochers/histograminteract', 'd3-format', 'ramda' ],
     output: [
         {
             format: 'umd',
@@ -19,6 +19,7 @@ export default {
         babel({
             exclude: 'node_modules/**',
         }),
+        postcss(),
         nodeResolve(),
         commonjs(),
         terser(),
